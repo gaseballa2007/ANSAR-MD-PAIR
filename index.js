b541ec9751e7f9b38f8932981d13a0760b59667d
PROxANSAR <panhwarking003@gmail.com>
2024-12-17 20:42:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
-    console.log(`Instagram: @um4rxd\n\nGitHub: @Um4r719\n\nServer running on http://localhost:${PORT}`);
+    console.log(`Instagram: @MrAnsar\n\nGitHub: @Panhwar110\n\nServer running on http://localhost:${PORT}`);
 });
 
 export default app;
